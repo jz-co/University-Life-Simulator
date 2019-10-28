8830459060d7288de6044a119f17f6504668f216
Jenny <jennny.zhang@mail.utoronto.ca>
2019-10-28 18:08:53 -0400

    Created the layout xml for the login page (activity_main.xml) and made comments for methods required in the MainActivity.java for the login page.

--- phase1/Game/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of the Presenter for this Activity
+    }
+
+    // Method for when the Login Button is clicked
+
+    void onLoginClick (View view){
+        // 1. Feed the username and password entries to Presenter
+        // 2. Get presenter to return whether or not the username exists
+        //    a. if the username exists, check if password is correct
+        //          i) If password is correct;
+        //              move to course selector activity
+        //          ii) If password is incorrect;
+        //              display error message
+        //    b. if username doesn't exist, display error, telling user to sign up
+
+    }
+
+    void onSignUpClick (View view) {
+        // 1. Check to see if username and password entries are empty (or space)
+        //      a. If empty, then display error message
+        //      b. Otherwise, feed username and password entries in to Presenter to have them create new user
+        //      If username and password entries are empty,
+        // 2. Move to customization page
     }
 }
