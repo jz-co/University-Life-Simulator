15c3fe4f31be75008470345e6fdf7d51099889f1
Jenny <jennny.zhang@mail.utoronto.ca>
2019-10-28 18:43:47 -0400

    Created 5 new activities stored under View folder and corresponding layout xml under layout folder.

---
+++ phase1/Game/app/src/main/java/com/example/game/View/CourseSelectorActivity.java
@@ -0,0 +1,16 @@
+package com.example.game.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.game.R;
+
+public class CourseSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_selector);
+    }
+}
---
+++ phase1/Game/app/src/main/java/com/example/game/View/FirstGameActivity.java
@@ -0,0 +1,16 @@
+package com.example.game.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.game.R;
+
+public class FirstGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_game);
+    }
+}
