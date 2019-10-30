03efbf08d09c61a046543583b28110376236d423
Z1Ranger <gvvarma2712@gmail.com>
2019-10-30 05:16:35 -0400

    "Created an abstract class for Lvl3 GameContents"

---
+++ phase1/Game/app/src/main/java/com/example/game/Model/Level3/GameContents.java
@@ -0,0 +1,47 @@
+package com.example.game.Model.Level3;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public abstract class GameContents {
+    protected int x;
+    protected int y;
+
+    Bitmap image;
+
+    /**
+     * The width of grid
+     */
+    protected int gridWidth;
+
+    /**
+     * The height of grid
+     */
+    protected int gridHeight;
+
+
+    public GameContents(Bitmap image, int gridWidth, int gridHeight) {
+        this.gridHeight = gridHeight;
+        this.gridWidth = gridWidth;
+        this.image = image;
+
+    }
+
+    abstract void draw(Canvas canvas);
+
+    abstract void update();
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {this.y = y;}
+
+    public void setX(int x) {this.x = x;}
+
+}
