8ff3f149fee2dd63b52a6930bbee42d3c8839da2
Z1Ranger <gvvarma2712@gmail.com>
2019-10-30 06:03:27 -0400

    "Created a wheel class which is a child class of Lvl3 GameContents"

---
+++ phase1/Game/app/src/main/java/com/example/game/Model/Level3/Wheel.java
@@ -0,0 +1,54 @@
+package com.example.game.Model.Level3;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class Wheel extends GameContents {
+
+    Bitmap image;
+    private String direction = "->";
+    private int speed;
+
+    Wheel(Bitmap image, int width, int height) {
+        super(image, width, height);
+        this.image = image;
+        super.setX((25 * width) / 100);
+        super.setY((15 * height) / 100);
+        speed = 5;
+    }
+
+    public int getWidth() {
+        return image.getWidth();
+    }
+
+    public int getHeight() {
+        return image.getHeight();
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, getX(), getY(), null);
+    }
+
+    public void update() {
+        if (!(x < gridWidth - image.getWidth() & x > 0)) {
+            reverse();
+        }
+        if (direction.equals("->")) {
+            x += speed;
+        } else {
+            x -= speed;
+        }
+    }
+
+    public void setSpeed() {
+        speed += 2;
+    }
+
+    public void reverse() {
+        if (direction.equals("->")) {
+            direction = "<-";
+        } else {
+            direction = "->";
+        }
+    }
+}
