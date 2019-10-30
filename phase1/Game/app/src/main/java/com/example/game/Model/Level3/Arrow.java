69f27d0e9bb7c72fce4d35ab1b138064e89fe803
Z1Ranger <gvvarma2712@gmail.com>
2019-10-30 05:49:06 -0400

    "Created a arrow class which is a child class of Lvl3 GameContents"

---
+++ phase1/Game/app/src/main/java/com/example/game/Model/Level3/Arrow.java
@@ -0,0 +1,25 @@
+package com.example.game.Model.Level3;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class Arrow extends GameContents {
+    Bitmap image;
+
+    Arrow(Bitmap image, int width, int height) {
+        super(image, width, height);
+        this.image = image;
+        super.setX(width - 570);
+        super.setY(height - 550);
+    }
+
+
+    void draw(Canvas canvas) {
+        canvas.drawBitmap(this.image, getX(), getY(), null);
+    }
+
+    void update() {
+        y -= 10;
+
+    }
+}
