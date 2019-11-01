7eb464b2dd5b68d7456c5305e3b082c99d525300
Grace Li <iigraceii.li@mail.utoronto.ca>
2019-11-01 00:12:33 -0400

    removed redundant private attibute student in GameLevel

--- phase1/Game/app/src/main/java/com/example/game/Model/GameLevel.java
+++ phase1/Game/app/src/main/java/com/example/game/Model/GameLevel.java
@@ -3,8 +3,6 @@ package com.example.game.Model;
 import com.example.game.Model.Student;
 
 public abstract class GameLevel {
-    private Student student;
-
     /**
      * The method to initiate the game.
      */
