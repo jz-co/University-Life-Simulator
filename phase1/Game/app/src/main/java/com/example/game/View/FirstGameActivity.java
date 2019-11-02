19f766bea7a3a4aaa3e12a2af37e8f9459238078
parinitaedke <parinita.u.edke@hotmail.com>
2019-11-01 21:56:44 -0400

    Added java docs to level 1 and hid the pause button

--- phase1/Game/app/src/main/java/com/example/game/View/FirstGameActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/View/FirstGameActivity.java
@@ -11,6 +11,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +26,7 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
     private TextView questionTV, correctTV, incorrectTV, hpTV, creditTV, gpaTV, resultTV;
     private EditText answerTV;
     private Button start, nextLevel, enter;
+    private ImageView pause;
     private boolean pauseGame = false;
     private String username;
     private CountDownTimer countDownTimer;
@@ -44,33 +46,46 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         setContentView(R.layout.activity_first_game);
 
         this.username = (String) getIntent().getSerializableExtra("Username");
+
         this.level1Presenter = new Level1Presenter(this, new DataHandler(this), username);
         this.questionTV = findViewById(R.id.question);
         this.correctTV = findViewById(R.id.correct);
         this.incorrectTV = findViewById(R.id.incorrect);
+
         this.creditTV = findViewById(R.id.creditLvl1);
         this.gpaTV = findViewById(R.id.gpaLvl1);
+        this.hpTV = findViewById(R.id.textView14);
+
         this.answerTV = findViewById(R.id.answer);
         this.resultTV = findViewById(R.id.resultLvl1);
+
         this.start = findViewById(R.id.startLv1);
         this.enter = findViewById(R.id.enterLv1);
         this.nextLevel = findViewById(R.id.nextLevelLvl1);
-        this.hpTV = findViewById(R.id.textView14);
-        this.creditTV = findViewById(R.id.creditLvl1);
-        this.gpaTV = findViewById(R.id.gpaLvl1);
 
+        this.pause = findViewById(R.id.pauseLvl1);
+
+        //set the game screen features invisible
         this.questionTV.setVisibility(View.INVISIBLE);
         this.correctTV.setVisibility(View.INVISIBLE);
         this.incorrectTV.setVisibility(View.INVISIBLE);
         this.answerTV.setVisibility(View.INVISIBLE);
         this.resultTV.setVisibility(View.INVISIBLE);
         this.enter.setVisibility(View.INVISIBLE);
+        this.pause.setVisibility(View.INVISIBLE);
 
         level1Presenter.initDisplay(this);
 
     }
 
+    /**
+     * Method called when the user clicks the start button
+     *
+     * @param view
+     */
     public void startGame(View view) {
+
+        //make game screen features visible
         this.start.setVisibility(View.INVISIBLE);
         this.nextLevel.setVisibility(View.INVISIBLE);
         this.questionTV.setVisibility(View.VISIBLE);
@@ -78,22 +93,34 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         this.incorrectTV.setVisibility(View.VISIBLE);
         this.answerTV.setVisibility(View.VISIBLE);
         this.enter.setVisibility(View.VISIBLE);
+        this.pause.setVisibility(View.VISIBLE);
 
         this.level1Presenter.startGame();
     }
 
+    /**
+     * Calls Level 2 Start page
+     */
     public void goToLevel2() {
         Intent intent = new Intent(this, Lvl2GameActivity.class);
         intent.putExtra("Username", this.username);
         startActivity(intent);
     }
 
-
-    public void displayWarning(String message){
+    /**
+     * Displays the warning message on the screen when the user enters an invalid entry
+     *
+     * @param message the warning message to be displayed
+     */
+    public void displayWarning(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-
+    /**
+     * Method called when the user presses the pause button
+     *
+     * @param view the view that this method is called from
+     */
     public void pauseOrResumeGame(View view) {
         if (pauseGame) {
             level1Presenter.resumeGame();
@@ -103,7 +130,12 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         this.pauseGame = !this.pauseGame;
     }
 
-    public void startTimer(long totalTime){
+    /**
+     * Timer for the game
+     *
+     * @param totalTime time for the game in milliseconds
+     */
+    public void startTimer(long totalTime) {
         countDownTimer = new CountDownTimer(totalTime, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -120,6 +152,11 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         }.start();
     }
 
+    /**
+     * Displays the remaining seconds on the screen
+     *
+     * @param secondsRemaining seconds remaining in the game.
+     */
     @SuppressLint("SetTextI18n")
     @Override
     public void setSecondsRemaining(long secondsRemaining) {
@@ -127,20 +164,41 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         seconds.setText("Secs Left:" + secondsRemaining);
     }
 
+    /**
+     * Displays the score for correct answers on the screen
+     *
+     * @param score the number of questions the student has correctly answered.
+     */
     @SuppressLint("SetTextI18n")
     public void displayCorrectScore(int score) {
         this.correctTV.setText("Correct: " + score);
     }
 
+    /**
+     * Displays the score for incorrect answers on the screen.
+     *
+     * @param score the number of questions the student has incorrectly answered.
+     */
     @SuppressLint("SetTextI18n")
     public void displayIncorrectScore(int score) {
         this.incorrectTV.setText("Incorrect: " + score);
     }
 
+    /**
+     * Displays the question on the screen.
+     *
+     * @param question the question to be displayed
+     */
     public void displayQuestion(String question) {
         this.questionTV.setText(question);
     }
 
+    /**
+     * Method called when the "Submit" button is clicked on the scree. The answer entered in the
+     * field is sent to the backend to be evaluated.
+     *
+     * @param view the view that calls this method
+     */
     public void evaluateAnswer(View view) {
         if (running) {
             String answerReceived = this.answerTV.getText().toString();
@@ -148,36 +206,72 @@ public class FirstGameActivity extends AppCompatActivity implements ILevel1.ILev
         }
     }
 
+    /**
+     * Sets the name of the player on the screen
+     *
+     * @param name name of the player
+     */
     @Override
     public void displayName(String name) {
         TextView tv_name = findViewById(R.id.Name);
         tv_name.setText(name);
     }
 
+    /**
+     * Sets the GPA of the player on the screen
+     *
+     * @param gpa GPA of the player
+     */
+    @SuppressLint("SetTextI18n")
     @Override
     public void displayGPA(double gpa) {
-        this.gpaTV.setText("gpa: "+ gpa);
+        this.gpaTV.setText("GPA: " + gpa);
     }
 
+    /**
+     * Displays the HP of the player on the screen
+     *
+     * @param hp HP of the player
+     */
     @Override
     public void displayHP(double hp) {
         String hpString = "HP: " + hp;
         this.hpTV.setText(hpString);
     }
 
+    /**
+     * Displays the number of credits of the player on the screen
+     *
+     * @param credit the credits of the player
+     */
+    @SuppressLint("SetTextI18n")
     @Override
     public void displayCredit(int credit) {
         this.creditTV.setText("credit: " + credit);
 
     }
 
+    /**
+     * Displays the final score at the end of the game.
+     */
+    @SuppressLint("SetTextI18n")
     public void endGame() {
         this.resultTV.setText("Score:" + Integer.toString(level1Presenter.getCorrectScore()));
         this.resultTV.setVisibility(View.VISIBLE);
         this.nextLevel.setVisibility(View.VISIBLE);
+
+        //set the question, answer views and the enter button invisible
+        this.questionTV.setVisibility(View.INVISIBLE);
+        this.answerTV.setVisibility(View.INVISIBLE);
+        this.enter.setVisibility(View.INVISIBLE);
     }
 
-    public void onClickNextLevel(View view){
+    /**
+     * Method called when the Next Level button is clicked on the screen.
+     *
+     * @param view the view that this method is called from.
+     */
+    public void onClickNextLevel(View view) {
         level1Presenter.validateLevel2();
     }
 }
