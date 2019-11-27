f4e15bbf957734ad3314a9ec870f78bc6b5be938
Grace Li <iigraceii.li@mail.utoronto.ca>
2019-11-26 21:08:59 -0500

    made some methods and variables protected instead of public.

--- phase2/UniversityLife/app/src/main/java/com/example/universitylife/LevelPresenter.java
+++ phase2/UniversityLife/app/src/main/java/com/example/universitylife/LevelPresenter.java
@@ -4,9 +4,9 @@ package com.example.universitylife;
 import com.example.universitylife.Student.StudentFacade;
 
 public abstract class LevelPresenter {
-    public GameManager gameManager;
+    protected GameManager gameManager;
 
-    public LevelPresenter(IData dataHandler, String username) {
+    protected LevelPresenter(IData dataHandler, String username) {
         this.gameManager = new GameManager(dataHandler, username);
     }
 
@@ -17,7 +17,7 @@ public abstract class LevelPresenter {
         updateDisplay(view);
     }
 
-    public void updateDisplay(ILevel.ILevelView view){
+    protected void updateDisplay(ILevel.ILevelView view) {
         StudentFacade currStudent = gameManager.getCurrentStudent();
         view.displayScore(currStudent.getGpa());
     }
