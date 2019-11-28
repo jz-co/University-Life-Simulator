e910e4e2ffb27d87a68376abbbe8d095b0280923
Grace Li <iigraceii.li@mail.utoronto.ca>
2019-11-27 21:16:59 -0500

    refactored student related classes. FacadeBuilder now builds the performance game-by-game.

--- phase2/UniversityLife/app/src/main/java/com/example/universitylife/Student/StudentPerformance.java
+++ phase2/UniversityLife/app/src/main/java/com/example/universitylife/Student/StudentPerformance.java
@@ -1,25 +1,79 @@
 package com.example.universitylife.Student;
 
+import java.util.Arrays;
+
 class StudentPerformance {
+    private int numberOfGames = 3;
+    private int numberOfLevels = 4;
+    private int numberOfBonusLevels = 3;
     private int credit;
     private double gpa = 0;
-    private int[] currentLevels = {1,1,1};
-    private double [][] scores = new double [3][3];
+    private int[] highestLevels = new int[numberOfBonusLevels];
+
+    private double[][] scores = new double[numberOfGames][numberOfLevels - numberOfBonusLevels];
+    // Bonus levels do not have a score associated with them.
 
     StudentPerformance(){
-
+        Arrays.fill(highestLevels, 1);
     }
 
-    StudentPerformance(int[] currentLevels, double[][] scores){
-        this.currentLevels = currentLevels;
-        this.scores = scores;
+    /**
+     * Records the student's performance in a game. (Used only when loading data of existing users)
+     *
+     * @param game         the index of the game, starting from 1
+     * @param highestLevel the highest level reached by the user, starting from 1
+     * @param scores       the scores the student received in each of the levels.
+     */
+    void setGamePerformance(int game, int highestLevel, double[] scores) {
+        highestLevels[game - 1] = highestLevel;
+        for (int level = 1; level <= numberOfLevels - numberOfBonusLevels; level++) {
+            setLevelScore(game, level, scores[level - 1]);
+        }
         updateCredit();
         updateGpa();
     }
 
-    private double calculateGameTotal(double[] scores){
-        double sum  = 0;
-        for (double s: scores){
+
+    /**
+     * Register the results of a non-bonus level. If the student has already completed this level,
+     * the highest level will not be increased, but the score will be registered if it is higher
+     * than the previous score.
+     *
+     * @param game  index of the game (>=1)
+     * @param level index of the level (>=1)
+     * @param score score received
+     */
+    void registerLevelResults(int game, int level, double score) {
+        if (highestLevels[game - 1] <= level) {
+            unlockNextLevel(game);
+        }
+        if (getLevelScore(game, level) < score) {
+            setLevelScore(game, level, score);
+        }
+    }
+
+    /**
+     * Register the results of a bonus level.
+     *
+     * @param game  index of the game
+     * @param level index of the level
+     */
+    void registerLevelResults(int game, int level) {
+        if (highestLevelOf(game) <= level) {
+            unlockNextLevel(game);
+        }
+    }
+
+
+    /**
+     * Calculate the total score for a game.
+     *
+     * @param game index of the game (>=1)
+     * @return total score for this game
+     */
+    private double calculateGameTotal(int game) {
+        double sum = 0;
+        for (double s: scores[game-1]){
             sum += s;
         }
         return sum;
@@ -28,9 +82,9 @@ class StudentPerformance {
 
     private void updateGpa() {
         double tempGpa = 0;
-        for (int game = 1; game <= 3; game++) {
-            if (currentLevels[game - 1] >= 3) {
-                tempGpa += calculateGameTotal(scores[game - 1]);
+        for (int game = 1; game <= numberOfGames; game++) {
+            if (gameIsCompleted(game)) {
+                tempGpa += calculateGameTotal(game);
             }
         }
         if (credit != 0) {
@@ -39,10 +93,10 @@ class StudentPerformance {
         this.gpa = tempGpa;
     }
 
-    private void updateCredit(){
+    private void updateCredit() {
         int credit = 0;
-        for (int game = 1; game<=3; game++){
-            if (currentLevels[game-1] >= 3){
+        for (int game = 1; game <= numberOfGames; game++) {
+            if (gameIsCompleted(game)){
                 credit ++;
             }
         }
@@ -50,34 +104,48 @@ class StudentPerformance {
     }
 
     boolean validateGraduation() {
-        return credit >= 3;
+        return credit >= numberOfGames;
     }
 
     int getCredit() {
         return credit;
     }
 
-    private void gameComplete(){
-        credit++;
-        updateGpa();
-    }
 
     double getGpa() {
         return gpa;
     }
 
-    public void unlockLevel(int game){
-        currentLevels[game-1]++;
-        if (currentLevels[game-1]>=3){
-            gameComplete();
+    /**
+     * Unlock the next level for the given game.
+     *
+     * @param game the index of teh game
+     */
+    private void unlockNextLevel(int game) {
+        highestLevels[game - 1]++;
+        if (highestLevelOf(game) == numberOfLevels - numberOfBonusLevels) {
+            updateCredit();
+            updateGpa();
+            // register game as complete when the user reaches the final non-bonus level.
         }
     }
 
-    public void setLevelScore(int game, int level, double score){
-        scores[game-1][level-1] = score;
+
+    private void setLevelScore(int game, int level, double score){
+        scores[game - 1][level - 1] = score;
     }
 
-    int getGameLevel(int game) {
-        return currentLevels[game-1];
+
+    double getLevelScore(int game, int level) {
+        return scores[game - 1][level - 1];
+    }
+
+
+    int highestLevelOf(int game){
+        return highestLevels[game - 1];
+    }
+
+    boolean gameIsCompleted(int game) {
+        return highestLevelOf(game) >= numberOfLevels - numberOfBonusLevels;
     }
 }
