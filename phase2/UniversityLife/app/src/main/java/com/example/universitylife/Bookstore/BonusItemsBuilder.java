12bb536bb7f92556e449695403aed8b75cb4fe02
Grace Li <iigraceii.li@mail.utoronto.ca>
2019-12-01 17:51:43 -0500

    debugged bookstore

--- phase2/UniversityLife/app/src/main/java/com/example/universitylife/Bookstore/BonusItemsBuilder.java
+++ phase2/UniversityLife/app/src/main/java/com/example/universitylife/Bookstore/BonusItemsBuilder.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class BonusItemsBuilder {
-    private ArrayList<BonusItem> masterList;
+    private ArrayList<BonusItem> masterList = new ArrayList<BonusItem>();
 
     void buildBonusItemsList() {
 
